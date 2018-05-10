1fb6c7d0ffb4ef047f234e5e99d32cb4a201d23c
TheSkyyDev <theskyydevelopment@gmail.com>
2018-05-10 23:22:48 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
 }
